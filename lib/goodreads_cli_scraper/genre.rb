8c0e69d2a44dd60b4ed75a8270eeea68111a3640
tsbrun <tsbrun@icloud.com>
2020-08-28 22:31:24 -0400

    Fleshed out logic of Scraper and Genre classes.

--- lib/goodreads_cli_scraper/genre.rb
+++ lib/goodreads_cli_scraper/genre.rb
@@ -1,9 +1,17 @@
 class GoodreadsCliScraper::Genre 
-    # attr_accessor :name 
+    # attr_accessor :name, :books
 
     # @@all = []
 
     # def self.all 
     #     @@all 
+    # end
+
+    # def save
+    #     @@all << self
+    #     @books = [] 
+    # end
+
+    # def print_books
     # end
 end
