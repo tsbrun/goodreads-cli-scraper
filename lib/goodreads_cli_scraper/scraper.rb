e4811f28edd8189ec249fc695957731179cbead5
tsbrun <tsbrun@icloud.com>
2020-09-01 16:12:05 -0400

    Built Scraper .get_page class method.

--- lib/goodreads_cli_scraper/scraper.rb
+++ lib/goodreads_cli_scraper/scraper.rb
@@ -1,8 +1,16 @@
+require 'Nokogiri'
+require 'open-uri'
+require 'pry'
+
+require '/Users/work/Documents/Flatiron/code/cli_project/goodreads_cli_scraper/lib/goodreads_cli_scraper.rb'
+
 class GoodreadsCliScraper::Scraper 
     # attr_accessor :genre 
 
-    # def get_page
-    # end
+    def self.get_page
+        Nokogiri::HTML(open("https://www.goodreads.com/genres"))
+    end
+    binding.pry
 
     # def get_genres
     # end
